52c8f64fb7087d9356b8034870f403adc9b451c6
jhenrique09 <jhsv09@gmail.com>
2022-06-08 12:58:01 +0000

    vendor: Fix app icon font on PixelLauncher
    Change-Id: Id98d40ff645d268d653f06d6761200d03ad7d668

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -153,6 +153,10 @@ PRODUCT_COPY_FILES += \
     vendor/aospa/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/aospa/target/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
 
+# Pixel
+PRODUCT_PACKAGES += \
+    CustomFontPixelLauncherOverlay 
+
 # Preinstalled Packages
 PRODUCT_COPY_FILES += \
     vendor/aospa/target/config/preinstalled-packages-aospa.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-aospa.xml
