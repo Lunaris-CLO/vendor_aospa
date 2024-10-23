dbecf53833e00c56dd4f61d89eb75abb30377dfe
Ghosuto <clash.raja10@gmail.com>
2024-10-23 08:23:53 +0000

    vendor: Dexopt NexusLauncherRelease
    Change-Id: Icc1ed04797bda03b6d381bfbf19183643eae3241

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -62,7 +62,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 DONT_DEXPREOPT_PREBUILTS := true
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    Launcher3QuickStep \
+    NexusLauncherRelease \
     ParanoidSystemUI \
     Settings 
 
