034308db37f01965a139de5298907f916d526b4d
Ghosuto <clash.raja10@gmail.com>
2024-11-06 09:55:00 +0000

    vendor: Don't build ParanoidSystemUI and ParanoidThemepicker
    Change-Id: I6a0cc3f49c1a5f7a91deae77ebdc45c45cd44c45

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -158,9 +158,7 @@ $(call inherit-product-if-exists, vendor/aospa/translations/translations.mk)
 # Paranoid Packages
 PRODUCT_PACKAGES += \
     ParanoidPapers \
-    ParanoidSettings \
-    ParanoidSystemUI \
-    ParanoidThemePicker
+    ParanoidSettings
 
 TARGET_DEFAULT_PIXEL_LAUNCHER ?= true
 ifeq ($(TARGET_DEFAULT_PIXEL_LAUNCHER), true)
