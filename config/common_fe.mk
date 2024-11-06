975b17879d6e2c93671843bd83cad06350cc3410
Ghosuto <clash.raja10@gmail.com>
2024-11-06 14:15:45 +0000

    vendor: Move some of our change to paranoid config
    Change-Id: I48fbd63ed89126afec55fd84d7bb6fb366206de0

--- config/common_fe.mk
+++ config/common_fe.mk
@@ -1,3 +1,53 @@
+# Apps
+PRODUCT_PACKAGES += \
+    AvatarPicker \
+    OmniStyle \
+    OmniJaws \
+    GameSpace 
+
+# blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+
+# Pixel Launcher
+TARGET_DEFAULT_PIXEL_LAUNCHER ?= true
+ifeq ($(strip $(TARGET_DEFAULT_PIXEL_LAUNCHER)),true)
+PRODUCT_PACKAGES += \
+    NexusLauncherRelease
+endif
+
+# Quick Switch
+ifeq ($(TARGET_DEFAULT_PIXEL_LAUNCHER), true)
+# Pixel Launcher
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.default_launcher=1 \
+    persist.sys.quickswitch_pixel_shipped=1
+else
+# Launcher3
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.default_launcher=0 \
+    persist.sys.quickswitch_pixel_shipped=0
+endif
+
+# Mainline spoofing
+    PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_mainline_BRAND?=google \
+    persist.sys.pihooks_mainline_DEVICE?=caiman \
+    persist.sys.pihooks_mainline_MANUFACTURER?=Google \
+    persist.sys.pihooks_mainline_PRODUCT?=caiman \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_DEVICE?=akita \
+    persist.sys.pihooks_PRODUCT?=akita_beta \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SECURITY_PATCH?=2024-10-05 \
+    persist.sys.pihooks_ID?=AP41.240925.009
+
+PIHOOKS_BUILD_GMS_FINGERPRINT := google/akita_beta/akita:15/AP41.240925.009/12534705:user/release-keys
+PIHOOKS_BUILD_FINGERPRINT := google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys
+PIHOOKS_MODEL_SPOOF := Pixel 9 Pro
+
+# TextClassifier
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/etc/textclassifier/actions_suggestions.universal.model \
     system/etc/textclassifier/lang_id.model \
