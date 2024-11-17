ef21dcf9a10d32ebf756cfac5eccdaebcec78c5e
Ghosuto <clash.raja10@gmail.com>
2024-11-17 12:17:34 +0000

    vendor: Update user and host name
    Change-Id: I2ae497766b8fb71d37e77adc08f5cb5a1dd6f07c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Shebang is intentionally missing - do not run as a script
 
 # Override host metadata to make builds more reproducible and avoid leaking info
-export BUILD_USERNAME=nobody
-export BUILD_HOSTNAME=android-build
+export BUILD_USERNAME=GHOST
+export BUILD_HOSTNAME=arcadia-build
