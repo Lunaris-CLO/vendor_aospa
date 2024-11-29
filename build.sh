xenxynon/android_vendor_aospa

build.sh: show ota name, size , sha256 and timestamp at the end of si… …gning
TODO: add args for delta too
Co-authored-by: ImSpiDy <SpiDy2713@gmail.com>

build.sh
@@ -186,6 +186,9 @@ AOSPA_VERSION="$(get_build_var AOSPA_VERSION)"
 checkExit
 echo -e ""
 
+# Custom Vars
+export OTA_FILE="PenguinOS-$AOSPA_VERSION.zip" 
+
 # Perform installclean, if requested so
 if [ "$FLAG_INSTALLCLEAN_BUILD" = 'y' ]; then
 	echo -e "${CLR_BLD_BLU}Cleaning compiled image files left from old builds${CLR_RST}"
@@ -226,7 +229,16 @@ elif [ "${KEY_MAPPINGS}" ]; then
         --block ${INCREMENTAL} \
         PenguinOS-$AOSPA_VERSION-signed-target_files-$FILE_NAME_TAG.zip \
         PenguinOS-$AOSPA_VERSION.zip
-
+    if [ -z "$FLAG_IMG_ZIP" ]; then
+        IMG_SIZE=$(ls -nl "$OTA_FILE" | awk '{print $5}')
+        IMG_SHA256=$(sha256sum "$OTA_FILE" | awk '{print $1}')
+	UTCSTAMP=$(grep 'ro.build.date.utc=' "$OUT"/system/build.prop | sed 's/^.*=//')
+	echo ""
+ 	echo "${CLR_BLD_GRN}OTA zip Complete:${CLR_RST} PenguinOS-$AOSPA_VERSION.zip${CLR_RST}"
+        echo "${CLR_BLD_GRN}SIZE:${CLR_RST} $IMG_SIZE bytes"
+        echo "${CLR_BLD_GRN}SHA256:${CLR_RST} $IMG_SHA256"
+	echo "${CLR_BLD_GRN}BUILD STAMP:${CLR_RST}: $UTCSTAMP"
+    fi
     checkExit
 
     if [ "$DELTA_TARGET_FILES" ]; then
@@ -247,8 +259,18 @@ elif [ "${KEY_MAPPINGS}" ]; then
         img_from_target_files \
             PenguinOS-$AOSPA_VERSION-signed-target_files-$FILE_NAME_TAG.zip \
             PenguinOS-$AOSPA_VERSION-image.zip
+	IMG_SIZE=$(ls -nl "$OTA_FILE" | awk '{print $5}')
+        IMG_SHA256=$(sha256sum "$OTA_FILE" | awk '{print $1}')
+	UTCSTAMP=$(grep 'ro.build.date.utc=' "$OUT"/system/build.prop | sed 's/^.*=//')
+	echo ""
+	echo "${CLR_BLD_GRN}Fastboot Zip:${CLR_RST} PenguinOS-$AOSPA_VERSION-img.zip"
+	echo "${CLR_BLD_GRN}OTA zip Complete:${CLR_RST} PenguinOS-$AOSPA_VERSION.zip${CLR_RST}"
+        echo "${CLR_BLD_GRN}SIZE:${CLR_RST} $IMG_SIZE bytes"
+        echo "${CLR_BLD_GRN}SHA256:${CLR_RST} $IMG_SHA256"
+        echo "${CLR_BLD_GRN}BUILD STAMP:${CLR_RST}: $UTCSTAMP"
         checkExit
     fi
+
 # Build rom package
 elif [ "$FLAG_IMG_ZIP" = 'y' ]; then
     m otatools target-files-package "$CMD"
@@ -275,11 +297,11 @@ else
     checkExit
 
     cp -f $OUT/aospa_$DEVICE-ota-$FILE_NAME_TAG.zip $OUT/PenguinOS-$AOSPA_VERSION.zip
-    echo "Package Complete: $OUT/PenguinOS-$AOSPA_VERSION.zip"
+    echo "OTA zip Complete: $OUT/PenguinOS-$AOSPA_VERSION.zip"
 fi
 echo -e ""
 
-# Check the finishing time
+# Check the finishing timestamp
 TIME_END=$(date +%s.%N)
 
 # Log those times at the end as a fun fact of the day

Lunaris-CLO/vendor_aospa

Signed-off-by: xenxynon <flynryder427@gmail.com>

build.sh
@@ -185,6 +185,9 @@ AOSPA_VERSION="$(get_build_var AOSPA_VERSION)"
 checkExit
 echo -e ""
 
+# Custom Vars
+export OTA_FILE="aospa-$AOSPA_VERSION.zip"
+
 # Perform installclean, if requested so
 if [ "$FLAG_INSTALLCLEAN_BUILD" = 'y' ]; then
 	echo -e "${CLR_BLD_BLU}Cleaning compiled image files left from old builds${CLR_RST}"
@@ -226,6 +229,16 @@ elif [ "${KEY_MAPPINGS}" ]; then
         Lunaris-OS-$AOSPA_VERSION-signed-target_files.zip \
         Lunaris-OS-$AOSPA_VERSION.zip
 
+    if [ -z "$FLAG_IMG_ZIP" ]; then
+        IMG_SIZE=$(ls -nl "$OTA_FILE" | awk '{print $5}')
+        IMG_SHA256=$(sha256sum "$OTA_FILE" | awk '{print $1}')
+	UTCSTAMP=$(grep 'ro.build.date.utc=' "$OUT"/system/build.prop | sed 's/^.*=//')
+	echo ""
+ 	echo "${CLR_BLD_GRN}OTA zip Complete:${CLR_RST} aospa-$AOSPA_VERSION.zip${CLR_RST}"
+        echo "${CLR_BLD_GRN}SIZE:${CLR_RST} $IMG_SIZE bytes"
+        echo "${CLR_BLD_GRN}SHA256:${CLR_RST} $IMG_SHA256"
+	echo "${CLR_BLD_GRN}BUILD STAMP:${CLR_RST}: $UTCSTAMP"
+    fi
     checkExit
 
     if [ "$DELTA_TARGET_FILES" ]; then
@@ -246,8 +259,18 @@ elif [ "${KEY_MAPPINGS}" ]; then
         img_from_target_files \
             Lunaris-OS-$AOSPA_VERSION-signed-target_files.zip \
             Lunaris-OS-$AOSPA_VERSION-image.zip
+	IMG_SIZE=$(ls -nl "$OTA_FILE" | awk '{print $5}')
+        IMG_SHA256=$(sha256sum "$OTA_FILE" | awk '{print $1}')
+	UTCSTAMP=$(grep 'ro.build.date.utc=' "$OUT"/system/build.prop | sed 's/^.*=//')
+	echo ""
+	echo "${CLR_BLD_GRN}Fastboot Zip:${CLR_RST} aospa-$AOSPA_VERSION-img.zip"
+	echo "${CLR_BLD_GRN}OTA zip Complete:${CLR_RST} aospa-$AOSPA_VERSION.zip${CLR_RST}"
+        echo "${CLR_BLD_GRN}SIZE:${CLR_RST} $IMG_SIZE bytes"
+        echo "${CLR_BLD_GRN}SHA256:${CLR_RST} $IMG_SHA256"
+        echo "${CLR_BLD_GRN}BUILD STAMP:${CLR_RST}: $UTCSTAMP"
         checkExit
     fi
+
 # Build rom package
 elif [ "$FLAG_IMG_ZIP" = 'y' ]; then
     m otatools target-files-package "$CMD"
@@ -273,12 +296,12 @@ else
 
     checkExit
 
-    cp -f $OUT/aospa_$DEVICE-ota.zip $OUT/Lunaris-OS-$AOSPA_VERSION.zip
-    echo "Package Complete: $OUT/Lunaris-OS-$AOSPA_VERSION.zip"
+    cp -f $OUT/aospa_$DEVICE-ota.zip $OUT/Lunaris-$AOSPA_VERSION.zip
+    echo "OTA zip Complete: $OUT/Lunaris-$AOSPA_VERSION.zip"
 fi
 echo -e ""
 
-# Check the finishing time
+# Check the finishing timestamp
 TIME_END=$(date +%s.%N)
 
 # Log those times at the end as a fun fact of the day