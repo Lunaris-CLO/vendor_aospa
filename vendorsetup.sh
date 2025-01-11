01364d4c881b141f6f2fbe6a593f18dc5dc0411a
Ghosuto <clash.raja10@gmail.com>
2025-01-11 18:54:24 +0000

    vendor: Setup thin lto cache
    Change-Id: Ie584bef473e0e3f4f794e208abcfc9a33669a489

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,6 @@
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=GHOST
 export BUILD_HOSTNAME=arcadia-build
+
+# performance.
+export USE_THINLTO_CACHE=true
