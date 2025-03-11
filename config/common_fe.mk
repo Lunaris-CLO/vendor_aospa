38ab7a598bfbb659b550151347da0a52c8dc66ae
minaripenguin <minaripenguin@users.noreply.github.com>
2025-03-11 04:44:47 +0000

    vendor: Disable memtag for different processes
    * it defaults to async if not specified

--- config/common_fe.mk
+++ config/common_fe.mk
@@ -151,6 +151,13 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.storage_manager.enabled=true
 
+# Disable async MTE on a few processes
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
+
 # Quick Tap
 PRODUCT_PACKAGES += \
     ColumbusService
