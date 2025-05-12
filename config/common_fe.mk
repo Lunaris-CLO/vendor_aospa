1335f9d5d41bfd991fc0e78859b7f052c7459860
Ghosuto <clash.raja10@gmail.com>
2025-05-12 17:17:37 +0000

    vendor: Disable package cache

--- config/common_fe.mk
+++ config/common_fe.mk
@@ -132,6 +132,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    pm.boot.disable_package_cache=true
+
 # Logging
 SPAMMY_LOG_TAGS := \
     SDM \
