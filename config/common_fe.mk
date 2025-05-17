b8f1e84de3a0e0caf04039931160df53d70dd997
Ionut <ghermanionut96@gmail.com>
2025-05-17 08:35:47 +0000

    vendor: update to the latest A16 beta BP22.250325.012

--- config/common_fe.mk
+++ config/common_fe.mk
@@ -104,9 +104,9 @@ $(call inherit-product-if-exists, packages/services/VncFlinger/product.mk)
     persist.sys.pihooks_PRODUCT?=tokay_beta \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
     persist.sys.pihooks_SECURITY_PATCH?=2025-04-05 \
-    persist.sys.pihooks_ID?=BP22.250325.007
+    persist.sys.pihooks_ID?=BP22.250325.012
 
-PIHOOKS_BUILD_GMS_FINGERPRINT := google/tokay_beta/tokay:16/BP22.250325.007/13352765:user/release-keys
+PIHOOKS_BUILD_GMS_FINGERPRINT := google/tokay_beta/tokay:16/BP22.250325.012/13467521:user/release-keys
 PIHOOKS_BUILD_GMS_MODEL := Pixel 9
 PIHOOKS_BUILD_FINGERPRINT := google/komodo/komodo:15/BP1A.250505.005/13277524:user/release-keys
 PIHOOKS_MODEL_SPOOF := Pixel 9 Pro XL
