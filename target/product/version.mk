8df89443bb47dd0d5bb2718b392f9e088ec1205b
Ghosuto <clash.raja10@gmail.com>
2025-06-01 05:20:34 +0000

    vendor: bump up version

--- target/product/version.mk
+++ target/product/version.mk
@@ -24,7 +24,7 @@
 # This is the global AOSPA version flavor that determines the focal point
 # behind our releases. This is bundled alongside $(AOSPA_MINOR_VERSION)
 # and only changes per major Android releases.
-AOSPA_MAJOR_VERSION := Radahn
+AOSPA_MAJOR_VERSION := Malenia
 
 # The version code is the upgradable portion during the cycle of
 # every major Android release. Each version code upgrade indicates
@@ -40,7 +40,7 @@ endif
 # Alpha: Development / Test releases
 # Beta: Public releases with CI
 # Stable: Final Product | No Tagging
-AOSPA_BUILD_VARIANT := 2.12
+AOSPA_BUILD_VARIANT := 2.13
 
 # Build Date
 BUILD_DATE := $(shell date -u '+%Y%m%d-%H%M')
